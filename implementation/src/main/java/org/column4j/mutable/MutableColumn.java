6fe7bcc73a7941fcc0dc9e461fe58acd4c250445
sibmaks <sibmaks@vk.com>
2023-10-25 21:13:44 +0300

    MutableColumn extends ColumnVector

--- implementation/src/main/java/org/column4j/mutable/MutableColumn.java
+++ implementation/src/main/java/org/column4j/mutable/MutableColumn.java
@@ -1,12 +1,13 @@
 package org.column4j.mutable;
 
 import org.column4j.Column;
+import org.column4j.ColumnVector;
 
 /**
  * @author sibmaks
  * @since 0.0.1
  */
-public interface MutableColumn<T> extends Column<T> {
+public interface MutableColumn<T> extends Column<T>, ColumnVector<T> {
     /**
      * Write tombstone into column at {@code position}
      *
