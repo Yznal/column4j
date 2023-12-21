4eeb3cd553e36d4454fd83f85b767747b35c19ac
vafilonov <sev.filonov@gmail.com>
2023-12-21 11:48:40 +0500

    other v3

---
+++ implementation/src/main/java/org/column4j/index/v3/chunk/primitive/impl/skip/Float64SkipIndex.java
@@ -0,0 +1,92 @@
+package org.column4j.index.v3.chunk.primitive.impl.skip;
+
+import org.column4j.index.v3.chunk.primitive.mutable.MutableFloat64ChunkIndex;
+import org.eclipse.collections.api.list.primitive.MutableIntList;
+import org.eclipse.collections.impl.list.mutable.primitive.IntArrayList;
+
+import javax.annotation.Nullable;
+
+public class Float64SkipIndex extends SkipChunkIndex implements MutableFloat64ChunkIndex {
+
+    private final double[] dataRef;
+
+    private final double[] segmentsMin;
+    private final double[] segmentsMax;
+
+    public Float64SkipIndex(double[] data, int segmentSize) {
+        super(segmentSize);
+        this.dataRef = data;
+
+        int segmentCount = data.length % segmentSize == 0
+                ? data.length / segmentSize
+                : data.length / segmentSize + 1;
+        int segmentIdx = segmentCount - 1;
+        this.segmentsMin = new double[segmentCount];
+        this.segmentsMax = new double[segmentCount];
+        double min = Double.MAX_VALUE;
+        double max = -Double.MAX_VALUE;
+        for (int i = data.length - 1; i >= 0 ; i--) {;
+            if (data[i] > max) {
+                max = data[i];
+            }
+            if (data[i] < min) {
+                min = data[i];
+            }
+            if (i % segmentSize == 0) {
+                segmentsMin[segmentIdx] = min;
+                segmentsMax[segmentIdx] = max;
+                min = Double.MAX_VALUE;
+                max = -Double.MAX_VALUE;
+            }
+        }
+    }
+
+    public Float64SkipIndex(double[] data) {
+        this(data, DEFAULT_SEGMENT_SIZE);
+    }
+
+    @Override
+    public boolean contains(double value) {
+        for (int s = 0; s < segmentsMin.length; s++) {
+            if (value >= segmentsMin[s] && value <= segmentsMax[s]) {
+                if (value == segmentsMin[s] || value == segmentsMax[s]){
+                    return true;
+                }
+                for (int i = s*segmentSize; i < s*segmentSize + 1 && i < dataRef.length; i++) {
+                    if (dataRef[i] == value) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public int[] lookupValues(double value) {
+        MutableIntList res = new IntArrayList();
+        for (int s = 0; s < segmentsMin.length; s++) {
+            if (value >= segmentsMin[s] && value <= segmentsMax[s]) {
+                for (int i = s*segmentSize; i < s*segmentSize + 1 && i < dataRef.length; i++) {
+                    if (dataRef[i] == value) {
+                        res.add(i);
+                    }
+                }
+            }
+        }
+        return res.isEmpty() ? null : res.toArray();
+    }
+
+    @Override
+    public void insertRecord(int offset, double value) {
+        int segment = offset / segmentSize;
+        if (value > segmentsMax[segment]) {
+            segmentsMax[segment] = value;
+        }
+        if (value < segmentsMin[segment]) {
+            segmentsMin[segment] = value;
+        }
+    }
+}
+
