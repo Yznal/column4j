bf5bab06e5986780ace4cf56e28484873db62a69
iv4n-t3a </bin/nologin>
2024-05-19 16:53:59 +0300

    Improve arrows benchmarks

--- arrows-benchmarks/src/main/java/org/column4jBench/arrows/AggregatorBenchmark.java
+++ arrows-benchmarks/src/main/java/org/column4jBench/arrows/AggregatorBenchmark.java
@@ -4,6 +4,7 @@ import org.openjdk.jmh.annotations.*;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.arrow.memory.BufferAllocator;
 import org.apache.arrow.memory.RootAllocator;
@@ -13,11 +14,17 @@ import org.apache.arrow.algorithm.sort.DefaultVectorComparators.IntComparator;
 import org.apache.arrow.algorithm.search.VectorSearcher;
 import org.apache.arrow.algorithm.search.ParallelSearcher;
 
+import org.openjdk.jmh.infra.Blackhole;
+
 /**
  * Benchmark for writing/reading data.
  */
-@Fork(1)
+@Fork(value = 3)
+@Measurement(iterations = 5, time = 10, timeUnit = TimeUnit.SECONDS)
+@Warmup(iterations = 3, time = 2, timeUnit = TimeUnit.SECONDS)
 @State(Scope.Thread)
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
 public class AggregatorBenchmark {
     @Param({"16", "128", "1024"})
     public int arraySize;
@@ -26,23 +33,34 @@ public class AggregatorBenchmark {
 
     RootAllocator alloc = new RootAllocator(Long.MAX_VALUE);
 
-    int defaultValue = 10;
-    int searchedValue = 11;
+    final IntComparator intcomparator = new IntComparator();
+
     IntVector vector;
     IntVector keyvector;
-    IntVector rarevector;
 
-    @Setup(Level.Iteration)
+    @Setup(Level.Invocation)
     public void setUp() {
+        if (vector != null) {
+            vector.close();
+        }
         vector = GenerateVector();
-        rarevector = GenerateRareVector();
         keyvector = CreateKeyVector();
     }
 
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
-    public void linearSearch() {
-        VectorSearcher.linearSearch(rarevector, new IntComparator(), keyvector, 0);
+    static public void linearSearch(Blackhole blackhole, AggregatorBenchmark benchmark) {
+        blackhole.consume(
+            VectorSearcher.linearSearch(benchmark.vector, benchmark.intcomparator, benchmark.keyvector, 0)
+        );
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    static public void liniarSearchFieldsInitialization(Blackhole blackhole, AggregatorBenchmark benchmark) {
+        blackhole.consume(
+            VectorSearcher.linearSearch(benchmark.vector, new IntComparator(), benchmark.CreateKeyVector(), 0)
+        );
     }
 
     private IntVector GenerateVector() {
@@ -55,21 +73,10 @@ public class AggregatorBenchmark {
         return vector;
     }
 
-    private IntVector GenerateRareVector() {
-        var vector = new IntVector("int vector", alloc);
-        vector.allocateNew(arraySize);
-        for (int i = 0; i < arraySize; ++i) {
-            vector.set(i, defaultValue);
-        }
-        vector.set(arraySize / 2, searchedValue);
-        vector.setValueCount(arraySize);
-        return vector;
-    }
-
     private IntVector CreateKeyVector() {
         var vector = new IntVector("int vector", alloc);
         vector.allocateNew(1);
-        vector.set(1, defaultValue);
+        vector.set(1, this.vector.get(arraySize / 2));
         vector.setValueCount(1);
         return vector;
     }
--- arrows-benchmarks/src/main/java/org/column4jBench/arrows/ReadWriteBenchmark.java
+++ arrows-benchmarks/src/main/java/org/column4jBench/arrows/ReadWriteBenchmark.java
@@ -4,56 +4,68 @@ import org.openjdk.jmh.annotations.*;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.arrow.memory.BufferAllocator;
 import org.apache.arrow.memory.RootAllocator;
 import org.apache.arrow.vector.IntVector;
 
+import org.openjdk.jmh.infra.Blackhole;
+
 /**
  * Benchmark for writing/reading data.
  */
-@Fork(1)
+@Fork(value = 3)
+@Measurement(iterations = 5, time = 10, timeUnit = TimeUnit.SECONDS)
+@Warmup(iterations = 3, time = 2, timeUnit = TimeUnit.SECONDS)
 @State(Scope.Thread)
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
 public class ReadWriteBenchmark {
     @Param({"16", "128", "1024"})
     public int arraySize;
 
     int[] array;
     RootAllocator alloc = new RootAllocator(Long.MAX_VALUE);
+    IntVector vector = new IntVector("int vector", alloc);
 
-    @Setup(Level.Iteration)
+    @Setup(Level.Invocation)
     public void setUp() {
+        if (vector != null) {
+            vector.close();
+        }
+        vector.allocateNew(arraySize);
         var rand = new Random();
         array = new int[arraySize];
         for (int i = 0; i < arraySize; ++i) {
-            array[i] = rand.nextInt(Integer.MAX_VALUE);
-        }
-    }
-
-    @Benchmark
-    @BenchmarkMode(Mode.Throughput)
-    public void writeArrow() {
-        var vector = createAndFillIntVector();
-        vector.close();
-    }
-
-    @Benchmark
-    @BenchmarkMode(Mode.Throughput)
-    public void writeReadArrow() {
-        var vector = createAndFillIntVector();
-        for (int i = 0; i < arraySize; ++i) {
-            int ignore = vector.get(i);
-        }
-        vector.close();
-    }
-
-    private IntVector createAndFillIntVector() {
-        var vector = new IntVector("int vector", alloc);
-        vector.allocateNew(arraySize);
-        for (int i = 0; i < arraySize; ++i) {
-            vector.set(i, array[i]);
+            array[i] = rand.nextInt();
+            vector.set(i, rand.nextInt());
         }
         vector.setValueCount(arraySize);
-        return vector;
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    static public void allocateArrow(Blackhole blackhole, ReadWriteBenchmark benchmark) {
+        var vector = new IntVector("int vector", benchmark.alloc);
+        vector.allocateNew(benchmark.arraySize);
+        blackhole.consume(vector);
+        vector.close();
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    static public void writeArrow(ReadWriteBenchmark benchmark) {
+        for (int i = 0; i < benchmark.arraySize; i++) {
+            benchmark.vector.set(i, benchmark.array[i]);
+        }
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    static public void readArrow(Blackhole blackhole, ReadWriteBenchmark benchmark) {
+        for (int i = 0; i < benchmark.arraySize; ++i) {
+            blackhole.consume(benchmark.vector.get(i));
+        }
     }
 }
